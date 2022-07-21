2315a136e6148e78dbc7eaf34fec5fc93da823de
Zachary Newman <zacharysnewman@icloud.com>
2022-07-21 15:37:49 -0700

    fix(triangle): Implement GetRandomPoint

--- Runtime/shapes/EquilateralTriangle.cs
+++ Runtime/shapes/EquilateralTriangle.cs
@@ -10,33 +10,31 @@ namespace Shaper.Shapes
         public float Height { get => Sqrt(Pow(this.Length, 2) - Pow(this.Length / 2, 2)); }
         public float Area { get => Sqrt(3f) / 4f * this.Length; }
         public float Perimeter { get => this.Length * 3f; }
-        public (float, float) P1 { get => (0, 0); }
-        public (float, float) P2 { get => (this.Length / 2, this.Height); }
-        public (float, float) P3 { get => (this.Length, 0); }
+        public (float, float) A { get => (0, 0); }
+		public (float, float) B { get => (this.Length, 0); }
+		public (float, float) C { get => (this.Length/2, this.Height); }
 
         public EquilateralTriangle(float length)
         {
             this.Length = length;
         }
 
-        // Source: https://blogs.sas.com/content/iml/2020/10/19/random-points-in-triangle.html
+        // Source: https://math.stackexchange.com/questions/3537762/random-point-in-a-triangle
         public (float, float) GetRandomPoint()
         {
-            var p1 = (0, 0);
-            var p2 = (this.Length / 2, this.Height);
-            var p3 = (this.Length, 0);
-            var a = VectorSubtract(p2, p1);
-            var b = VectorSubtract(p3, p1);
+            var(Cx, Cy) = this.C;
+			var r1 = Randomf.Range(0f,1f);
+			var r2 = Randomf.Range(0f,1f);
 
-            var u1 = Randomf.Range(0f, 1f);
-            var u2 = Randomf.Range(0f, 1f);
-            if(u1 + u2 > 1)
-            {
-                u1 = 1 - u1;
-                u2 = 1 - u2;
-            }
-            //var w = u1 * a + u2 * b;
-            throw new NotImplementedException();
+			// if over the diagonal, then flip r1 & r2
+			if (r1 + r2 > 1) {
+				r1 = (1 - r1);
+				r2 = (1 - r2);
+			}
+
+			var x = this.Length * r2 + Cx * r1;
+			var y = Cy * r1;
+			return (x, y);
         }
     }
 }
